8c1239d0a0edfb72a7156da400ec11529f778b95
cabrera-alejandr26 <cabrera.alejandr26@gmail.com>
2021-09-28 15:51:10 -0700

    done for the day

---
+++ src/com/amigoscode/JavaII/ArrPractice.java
@@ -0,0 +1,71 @@
+package com.amigoscode.JavaII;
+
+import java.util.Arrays;
+
+import static java.util.Arrays.fill;
+
+public class ArrPractice {
+    public static void main(String[] args) {
+        int[] numbers = new int[3];
+        numbers[0] = 1;
+        numbers[1] = 2;
+
+        System.out.println(numbers[0]); // 1
+        System.out.println(numbers[1]); // 2
+        System.out.println(numbers[2]); // 0 -- default value
+//        System.out.println(numbers[3]); // ArrayIndexOutOfBoundsException !!!
+
+        // using the array initializer syntax
+        String[] beatles = {"John", "Paul", "Ringo"};
+
+        System.out.println(beatles.length); // 3
+
+        System.out.println(beatles[0]); // "John"
+        System.out.println(beatles[1]); // "Paul"
+        System.out.println(beatles[2]); // "Ringo"
+//
+//// ArrayIndexOutOfBoundsException !
+//        beatles[3] = "George";
+
+//        String[] languages = {"html","css","javascript","java"};
+//
+//        for (int i = 0; i < languages.length; i +=1){
+//            System.out.println(languages[i]);
+//        }
+        //Enhanced for loop:
+        String[] languages = {"html", "css", "javascript", "java"};
+
+        for (String language : languages) {
+            System.out.println(language);
+        }
+
+       Arrays.fill(languages, "hello");
+        for (String language : languages) {
+            System.out.println(language);
+        }
+
+        int[][] matrix = {
+                {1, 2, 3},
+                {4, 5, 6},
+                {7, 8, 9}
+        };
+
+        for (int[] row : matrix) {
+            System.out.println("+---+---+---+");
+
+            System.out.print("| ");
+            for (int n : row) {
+                System.out.print(n + " | ");
+            }
+
+            System.out.println();
+        }
+
+
+
+    }
+
+
+
+
+}
